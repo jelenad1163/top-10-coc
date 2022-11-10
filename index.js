c5f7233dd8687c8251e92908d2881d86b3ecdaf2
Jelena Djurdjevic <+>
2022-11-10 15:48:21 -0700

    add fetch cocktails

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 document.addEventListener('DOMContentLoaded', () =>{
     const cocktailBtn = document.querySelector("#cocktails").addEventListener('click', getCocktails)
-    getCocktails()
+    //getCocktails()
+    fetchCocktails()
    })
+let cocktails
+function fetchCocktails(){
+    fetch("http://localhost:3000/cocktails")
+        .then(res => res.json())
+        .then(data => {
+            cocktails = data
+        })  
+}
 
 const displayCocktail = (event) => {
-const cocktailInfo = document.querySelector("#cocktail-info")
-const mainCocktailsUl= document.querySelector("#cocktail-list")
-mainCocktailsUl.innerHTML = ''
+    const cocktailInfo = document.querySelector("#cocktail-info")
+    const mainCocktailsUl= document.querySelector("#cocktail-list")
+    mainCocktailsUl.innerHTML = ''
     fetch("http://localhost:3000/cocktails")
         .then(res => res.json())
         .then(data => {
             data.forEach(cocktail => {
                 if(cocktail.id == event.target.id){
-                cocktailInfo.innerHTML += "<h2>"+cocktail.name +"</h2>"
+                cocktailInfo.innerHTML += `<h2>${cocktail.name}</h2>`
                 cocktailInfo.innerHTML += "<h3>Ingredients</h3>"
-                cocktailInfo.innerHTML += "<ul>"
+                //cocktailInfo.innerHTML += `<ol type="1">`
                 cocktail.ingredients.forEach(i => {
-                    cocktailInfo.innerHTML += "<li>" + i + "</li>"
+                    cocktailInfo.innerHTML += `<li>${i}</li>`
                 })
-                cocktailInfo.innerHTML += "</ul>"
+                //cocktailInfo.innerHTML += "</ol>"
                 cocktailInfo.innerHTML += "<h3>Steps</h3>"
-                cocktailInfo.innerHTML += "<ol>"
+                //cocktailInfo.innerHTML += "<ol>"
                 cocktail.steps.forEach(i => {
-                    cocktailInfo.innerHTML += "<li>" + i + "</li>"
+                    cocktailInfo.innerHTML += `<li>${i}</li>`
                 })
-                cocktailInfo.innerHTML += "</ol>"
-                cocktailInfo.innerHTML += "<img src='" + cocktail.image+"'>"
+                //cocktailInfo.innerHTML += "</ol>"
+                cocktailInfo.innerHTML += `<img src= "${cocktail.image}">`
                 }
             }
             )
@@ -38,22 +47,23 @@ function getCocktails(){
     const cocktailInfo = document.querySelector("#cocktail-info")
     cocktailInfo.innerHTML = ''
     mainCocktailsUl.innerHTML = ''
-    fetch("http://localhost:3000/cocktails")
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(cocktail => {
+    console.log(cocktails)
+    
+        cocktails.forEach(cocktail => {
+            console.log(cocktail)
             mainCocktailsUl.innerHTML += `
                 <li>${cocktail.name}
                 <button id= "${cocktail.id}">Get ${cocktail.name} Recipe</button></li>
                 `
                 attachClicksToBtn()
             }
-        )}
-    )}
+        )
+        debugger;
+}
                 
 const attachClicksToBtn = () =>{
-    const cocktails = document.querySelectorAll('button')
-    cocktails.forEach((cocktail) => {
+    const cocktailButtons = document.querySelectorAll('button')
+    cocktailButtons.forEach((cocktail) => {
         cocktail.addEventListener('click', displayCocktail)
         cocktail.addEventListener('mouseover', () =>{
             cocktail.style.color= 'white'
